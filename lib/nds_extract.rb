32171c17dba04aaebd67ab4abbee2e31297c3254
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-06 19:41:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,7 +40,7 @@ end
 def total_gross(source)
   index = 0
   grand_total = 0
-    while index < list_of_directors(source).lenght do
+    while index < list_of_directors(source).length do
       director = list_of_directors[source]
       total+=directors_totals(director)
       index+=1
