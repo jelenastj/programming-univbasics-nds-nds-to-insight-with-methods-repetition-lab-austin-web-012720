e3e2cbb801ebda367bbca59e70722b79f0fb1dfa
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-06 20:30:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,13 +38,17 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  index = 0
-  grand_total = 0
-    while index < directors_totals[index].length do
-      total+=directors_totals[index]
-      index+=1
-    end
-grand_total
+ director_earning_hash = directors_totals(source)
+  director_names = list_of_directors(source)
+  i = 0
+  total = 0
+
+  while i < director_names.length do
+    dir_name = director_names[i]
+    total += director_earning_hash[dir_name]
+    i += 1
+end
+total
 end 
 
   
